bcc538969f7f603488d1e36d4e2c1cf6eaed0aca
grydholt <jacob@grydholt.dk>
2014-11-02 17:21:08 +0100

    best.R implementation + rankhospital.R skeleton

--- best.R
+++ best.R
@@ -1,6 +1,30 @@
 best <- function(state, outcome) {
+        data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        
+        if (state %in% data$State) {
+                        
+        } else {
+                stop("invalid state")
+        }
+                
+        if (outcome == "heart attack") {
+                column <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+        } else if (outcome == "heart failure") {
+                column <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+        } else if (outcome == "pneumonia") {
+                column <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+        } else {
+                stop("invalid outcome")
+        }
+        
+        
         ## Read outcome data
         ## Check that state and outcome are valid
         ## Return hospital name in that state with lowest 30-day death
         ## rate
+        narrow = data[, c("State", "Hospital.Name", column)]
+        narrow[,column] = suppressWarnings(as.numeric(narrow[,column]))
+        narrow = narrow[narrow$State==state,]
+        ordered = narrow[order(narrow[,3], narrow[,2]),]
+        ordered[1,]$Hospital.Name
 }
--- snippets.R
+++ snippets.R
@@ -1,2 +1,3 @@
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 head(outcome)
+
